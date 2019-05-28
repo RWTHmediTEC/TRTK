105486cf334d27f068053c409ff5224b0f25211c
Christoph Hänisch <haenisch@surgitaix.com>
2019-05-28 16:48:27 +0200

    Enhanced documentation for the PinholeCameraModel class. Adapted the example code.

--- examples/pinhole_camera_model_1.cpp
+++ examples/pinhole_camera_model_1.cpp
@@ -1,11 +1,7 @@
-// Last changed on 2018-05-17.
+// Last changed on 2018-05-28.
 
-#include <cmath>
-#include <cstdlib>
 #include <iostream>
 #include <iomanip>
-#include <tuple>
-#include <utility>
 #include <vector>
 
 #include<Eigen/Core>
@@ -22,151 +18,70 @@ using namespace std;
 using namespace TRTK;
 using namespace TRTK::Tools;
 
-double pi = 3.1415926535;
-
-using CameraModel = PinholeCameraModel<double>;
-using value_type = CameraModel::value_type ;
-using Point3D = CameraModel::Vector3T;
-using Point2D = CameraModel::Vector2T;
+using Point3D = PinholeCameraModel<double>::Vector3T;
+using Point2D = PinholeCameraModel<double>::Vector2T;
 
 
-// Note, PinholeCameraModel<T>::transform() and PinholeCameraModel<T>::operator() were
-// manually checked and seem to works correctly (either way, these are currently only
-// one-liners). Thus, we will make use of them and assume their correctness.
-
-
-namespace
+struct GenerateTestData
 {
-
-    struct GenerateTestData
+    GenerateTestData()
     {
-        GenerateTestData(double sigma = 0)
+        // Instantiate a pinhole camera model.
+
+        PinholeCameraModel<double> camera_model;
+
+        // Set the intrinsic camera parameters.
+
+        camera_model.setFocalLengths(80, 79);
+        camera_model.setImageCenter(250, 251);
+        camera_model.setSkew(0.1);
+
+        // Set the extrinsic camera parameters.
+
+        Transform3D<double> T;
+        T.rotateAxis(40, Point3D(1, 2, 3), Transform3D<double>::DEGREES);
+        T.translate(130, 135, 90);
+        camera_model.setExtrinsicParameters(T.getTransformationMatrix());
+
+        // Generate some test data.
+
+        const int number_of_points = 6;
+
+        for (int i = 0; i < number_of_points; ++i) // generate points in the world COS
         {
-            // Instantiate a pinhole camera model.
-
-            CameraModel camera_model;
-            camera_model.setFocalLengths(10, 10);
-
-            cout << "Intrinsic camera parameters: " << endl << camera_model.getIntrinsicParameters() << endl;
-            cout << "Extrinsic camera parameters: " << endl << camera_model.getExtrinsicParameters() << endl;
-
-            // Generate some (noisy) test data.
-
-            points_world.emplace_back(0, 0, 100); // mapped to (0, 0) in the image plane
-            points_world.emplace_back(10, 10, 100); // mapped to (1, 1) in the image plane
-            points_world.emplace_back(20, -10, 80); // mapped to (2.5, -1.25) in the image plane
-            points_world.emplace_back(15, -10, 90);
-            points_world.emplace_back(-20, 10, 100);
-            points_world.emplace_back(-20, -20, 80);
-
-            for (auto const & p_W : points_world)
-            {
-                Point2D p_D = camera_model.transform(p_W);
-                points_display.push_back(p_D);
-            }
-
-            // Add noise to the points recorded in the world COS.
-
-            for (auto & p_W : points_world)
-            {
-                p_W += Point3D(randn(0.0, sigma), randn(0.0, sigma), randn(0.0, sigma));
-            }
+            auto x = randn(0.0, 30.0);    // in the camera COS
+            auto y = randn(0.0, 30.0);    // in the camera COS
+            auto z = randn(600.0, 10.0);  // in the camera COS
+            Point3D p = T.inverse() * Point3D(x, y, z);  // in the world COS
+            points_world.emplace_back(p);
         }
 
-        vector<Point3D> points_world;
-        vector<Point2D> points_display; // image plane
-    };
-
-
-    struct GenerateTestData2
-    {
-        GenerateTestData2(double sigma = 0)
+        for (auto const & p_W : points_world) // generate points in the camera COS
         {
-            // Instantiate a pinhole camera model.
-
-            CameraModel camera_model;
-
-            // Set the intrinsic camera parameters.
-
-            camera_model.setFocalLengths(80, 80);
-            camera_model.setImageCenter(0, 0);
-            camera_model.setSkew(0);
-
-            // Set the extrinsic camera parameters.
-
-            Transform3D<double> T;
-            T.rotateAxis(0, Point3D(1, 1, 1), Transform3D<double>::DEGREES);
-            T.translate(130, 135, 90);
-            // camera_model.setExtrinsicParameters(T.getTransformationMatrix());
-
-            cout << "Intrinsic camera parameters: " << endl << camera_model.getIntrinsicParameters() << endl;
-            cout << "Extrinsic camera parameters: " << endl << camera_model.getExtrinsicParameters() << endl;
-
-            // Generate some test data.
-
-            const int number_of_points = 12;
-
-            for (int i = 0; i < number_of_points; ++i) // points in the world COS
-            {
-                auto x = randn(0.0, 2.0);
-                auto y = randn(50.0, 2.0);
-                auto z = randn(600.0, 10.0);
-                points_world.emplace_back(Point3D(x, y, z));
-            }
-
-            for (auto const & p_W : points_world) // points in the display COS
-            {
-                Point2D p_D = camera_model.transform(p_W) + Point2D(randn(0.0, sigma), randn(0.0, sigma));
-                points_display.push_back(p_D);
-            }
-
-            // Add noise to the points recorded in the world COS.
-
-            for (auto & p_W : points_world)
-            {
-                p_W += Point3D(randn(0.0, sigma), randn(0.0, sigma), randn(0.0, sigma));
-            }
+            Point2D p_D = camera_model.transform(p_W);
+            points_display.push_back(p_D);
         }
+    }
 
-        vector<Point3D> points_world;
-        vector<Point2D> points_display; // image plane
-    };
-
-} // end of anonymous namespace
+    vector<Point3D> points_world;
+    vector<Point2D> points_display; // image plane
+};
 
 
 int main()
 {
+
+    srand(0);
+    GenerateTestData test_data;
+
+    PinholeCameraModel<double> model;
+    auto rmse = model.estimate(make_range(test_data.points_world), make_range(test_data.points_display));
+
     cout << setprecision(4);
     cout << fixed;
-
-    // Matrix3d A = (Matrix3d() << 1, 2, 3, 0, 4, 5, 0, 0, 6).finished();
-    // Matrix3d Q, R;
-    // tie(Q, R) = computeRQDecomposition<double>(A);
-    // cout << "A:" << endl << A << endl << endl;
-    // cout << "Q:" << endl << Q << endl << endl;
-    // cout << "R:" << endl << R << endl << endl;
-    // cout << "R * Q:" << endl << R * Q << endl << endl;
-
-
-    cout << "Pinhole camera model example" << endl;
-    cout << "----------------------------" << endl << endl;
-
-    {
-        srand(0);
-        GenerateTestData2 test_data(0.1);
-
-        CameraModel model;
-        auto rmse = model.estimate(make_range(test_data.points_world), make_range(test_data.points_display));
-
-        cout << "No noise" << endl;
-        cout << "RMSE: " << rmse << endl;
-        cout << "Extrinsic camera parameters: " << endl << model.getExtrinsicParameters() << endl;
-        cout << "Intrinsic camera parameters: " << endl << model.getIntrinsicParameters() << endl;
-    }
-
-    cout << endl;
+    cout << "RMSE: " << rmse << endl << endl;
+    cout << "Extrinsic camera parameters: " << endl << model.getExtrinsicParameters() << endl << endl;
+    cout << "Intrinsic camera parameters: " << endl << model.getIntrinsicParameters() << endl;
 
     return 0;
 }
-
--- include/TRTK/PinholeCameraModel.hpp
+++ include/TRTK/PinholeCameraModel.hpp
@@ -19,6 +19,7 @@
 #define PINHOLE_CAMERA_MODEL_HPP_4231789408
 
 
+#include <iostream> // TODO: DELETE ME
 #include <tuple>
 #include <utility>
 #include <vector>
@@ -188,6 +189,117 @@ namespace TRTK
      *
      * For more details, please be referred to [1], [2], and [3].
      *
+     * \par Example:
+     *
+     * \code
+     *
+     * #include <iostream>
+     * #include <iomanip>
+     * #include <vector>
+     *
+     * #include<Eigen/Core>
+     * #include<Eigen/StdVector>
+     *
+     * #include <TRTK/Coordinate.hpp>
+     * #include <TRTK/PinholeCameraModel.hpp>
+     * #include <TRTK/Tools.hpp>
+     * #include <TRTK/Transform3D.hpp>
+     *
+     *
+     * using namespace Eigen;
+     * using namespace std;
+     * using namespace TRTK;
+     * using namespace TRTK::Tools;
+     *
+     * using Point3D = PinholeCameraModel<double>::Vector3T;
+     * using Point2D = PinholeCameraModel<double>::Vector2T;
+     *
+     *
+     * struct GenerateTestData
+     * {
+     *     GenerateTestData()
+     *     {
+     *         // Instantiate a pinhole camera model.
+     *
+     *         PinholeCameraModel<double> camera_model;
+     *
+     *         // Set the intrinsic camera parameters.
+     *
+     *         camera_model.setFocalLengths(80, 79);
+     *         camera_model.setImageCenter(250, 251);
+     *         camera_model.setSkew(0.1);
+     *
+     *         // Set the extrinsic camera parameters.
+     *
+     *         Transform3D<double> T;
+     *         T.rotateAxis(40, Point3D(1, 2, 3), Transform3D<double>::DEGREES);
+     *         T.translate(130, 135, 90);
+     *         camera_model.setExtrinsicParameters(T.getTransformationMatrix());
+     *
+     *         // Generate some test data.
+     *
+     *         const int number_of_points = 6;
+     *
+     *         for (int i = 0; i < number_of_points; ++i) // generate points in the world COS
+     *         {
+     *             auto x = randn(0.0, 30.0);    // in the camera COS
+     *             auto y = randn(0.0, 30.0);    // in the camera COS
+     *             auto z = randn(600.0, 10.0);  // in the camera COS
+     *             Point3D p = T.inverse() * Point3D(x, y, z);  // in the world COS
+     *             points_world.emplace_back(p);
+     *         }
+     *
+     *         for (auto const & p_W : points_world) // generate points in the camera COS
+     *         {
+     *             Point2D p_D = camera_model.transform(p_W);
+     *             points_display.push_back(p_D);
+     *         }
+     *     }
+     *
+     *     vector<Point3D> points_world;
+     *     vector<Point2D> points_display; // image plane
+     * };
+     *
+     *
+     * int main()
+     * {
+     *
+     *     srand(0);
+     *     GenerateTestData test_data;
+     *
+     *     PinholeCameraModel<double> model;
+     *     auto rmse = model.estimate(make_range(test_data.points_world), make_range(test_data.points_display));
+     *
+     *     cout << setprecision(4);
+     *     cout << fixed;
+     *     cout << "RMSE: " << rmse << endl << endl;
+     *     cout << "Extrinsic camera parameters: " << endl << model.getExtrinsicParameters() << endl << endl;
+     *     cout << "Intrinsic camera parameters: " << endl << model.getIntrinsicParameters() << endl;
+     *
+     *     return 0;
+     * }
+     *
+     * \endcode
+     *
+     * \par Output:
+     *
+     * \verbatim
+
+       RMSE: 0.0000
+
+       Extrinsic camera parameters:
+         0.7828  -0.4820   0.3937 130.0000
+         0.5488   0.8329  -0.0715 135.0000
+        -0.2935   0.2721   0.9164  90.0000
+         0.0000   0.0000   0.0000   1.0000
+
+       Intrinsic camera parameters:
+        80.0000   0.1000 250.0000   0.0000
+         0.0000  79.0000 251.0000   0.0000
+         0.0000   0.0000   1.0000   0.0000
+
+       \endverbatim
+     *
      * \references
      *
      * [1] Hartley and Zisserman, "Multiple view geometry in computer vision", 2003
@@ -201,7 +313,7 @@ namespace TRTK
      *
      * \author Christoph Hänisch
      * \version 1.0.0
-     * \date last changed on 2019-05-20
+     * \date last changed on 2019-05-28
      */
 
     template <class T>
